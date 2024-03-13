3a787d7363fef3c8b1278afa69d51b2554c4a8ca
orivero83 <orivero83g@gmail.com>
2024-03-13 12:54:19 -0500

    puerto
    puerto

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000; 
 
 app.use(express.json());
 
@@ -78,9 +79,7 @@ app.post('/transacciones', (req, res) => {
 });
 
 
-
-
-
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
+
