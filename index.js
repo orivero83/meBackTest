19bc679b4b3fc8e4788a36f1aeeebf044aa90ba0
orivero83 <orivero83g@gmail.com>
2024-03-13 15:35:48 -0500

    Middleware de conexión a la base de datos
     Middleware de conexión a la base de datos

--- index.js
+++ index.js
@@ -7,43 +7,45 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
 // Usa cors con opciones específicas
 const frontendURL = 'https://mercadopagotest.medidasdigitales.com';
-app.use(cors({
-  origin: frontendURL,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: frontendURL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+);
 
+// Middleware de conexión a la base de datos
+app.use((req, res, next) => {
+  // Configuración de la conexión a la base de datos
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
 
-// Configuración de la conexión a la base de datos
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Conectado a la base de datos MySQL');
+      req.dbConnection = connection; // Adjuntar la conexión al objeto de solicitud
+      next(); // Continuar con la siguiente middleware/ruta
+    }
+  });
 });
 
-
-// Conectar a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-    return;
-  }
-  console.log('Conectado a la base de datos MySQL');
-});
-
-
-
-// Ruta para crear una nueva transacción (GET)
 // Ruta de prueba para obtener datos de la base de datos
 app.get('/transacciones', (req, res) => {
+  const connection = req.dbConnection; // Obtener la conexión desde el objeto de solicitud
   connection.query('SELECT * FROM transacciones', (err, results) => {
     if (err) {
       console.error('Error al ejecutar la consulta:', err);
@@ -54,16 +56,24 @@ app.get('/transacciones', (req, res) => {
   });
 });
 
-
-
-
 // Ruta para crear una nueva transacción (POST)
 app.post('/transacciones', (req, res) => {
+  const connection = req.dbConnection; // Obtener la conexión desde el objeto de solicitud
+
   try {
     const nuevaTransaccion = req.body;
 
     // Realizar la inserción en la base de datos
-    const { tipo_pago, estado, monto, moneda, id_pago, id_usuario, nombre_persona, email_persona } = nuevaTransaccion;
+    const {
+      tipo_pago,
+      estado,
+      monto,
+      moneda,
+      id_pago,
+      id_usuario,
+      nombre_persona,
+      email_persona,
+    } = nuevaTransaccion;
 
     connection.query(
       'INSERT INTO transacciones (tipo_pago, estado, monto, moneda, id_pago, id_usuario, nombre_persona, email_persona) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
@@ -81,14 +91,13 @@ app.post('/transacciones', (req, res) => {
         } else {
           res.status(500).send('Error al procesar la solicitud');
         }
-      });
+      }
+    );
   } catch (error) {
     console.error('Error al procesar la solicitud:', error);
     res.status(500).send('Internal Server Error');
   }
 });
-
-
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en port: ${port}`);
